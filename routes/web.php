fd73a07e87f5c463c4e397a31d4c6aa3a81cbc88
Ery <eryutami08@gmail.com>
2026-07-30 19:35:16 +0700

    Menambahkan route untuk post, like, dan komentar

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,6 +17,15 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::resource('posts', PostController::class);
+    Route::post('/posts/{post}/like', [LikeController::class, 'store'])
+        ->name('posts.like');
+
+    Route::delete('/posts/{post}/like', [LikeController::class, 'destroy'])
+        ->name('posts.unlike');
+
+    Route::post('/posts/{post}/comment', [CommentController::class, 'store'])
+        ->name('comments.store');
+        
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
